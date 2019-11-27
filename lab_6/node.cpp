c1cf3c25415fde48a34df88da9c051cd41071d2c
Bartosz Piętka <57501043+barpje@users.noreply.github.com>
2019-11-27 20:50:47 +0100

    Update node.cpp

--- lab_6/node.cpp
+++ lab_6/node.cpp
@@ -4,7 +4,11 @@
 using namespace std;
 
 unsigned Node::_suma=0;
-
+ Node::Node(const Coin& tmp){
+    coin_ptr=new Coin(tmp);
+    _suma+=coin_ptr->get_coin_value(); 
+ };
+ 
 unsigned Node::get_tot_coins_val(){
    return _suma;
 };
