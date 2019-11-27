3e6ccf76f30843a0e6e284c30b8f63281ee062ea
Bartosz Piętka <57501043+barpje@users.noreply.github.com>
2019-11-27 20:53:09 +0100

    Update stack.cpp

--- lab_6/stack.cpp
+++ lab_6/stack.cpp
@@ -6,37 +6,36 @@
 using namespace std; 
 
 
-Stack::Stack(const Coin& coin_ptr){
-    head = new Node;
-    head->coin_ptr=coin_ptr;
+Stack::Stack(const Coin& coin){
+    head = new Node(coin);
     head->next=NULL;
-    head->_suma+=coin_ptr.get_coin_value();
 };
 void Stack::push(const Coin& coin_ptr){
-    Node *nas=new Node;
-    nas->coin_ptr=coin_ptr;
+    Node *nas=new Node(coin_ptr);
     nas->next=NULL;
     if(head == NULL)
 		nas->next = NULL;
 	else
 		nas->next = head;
 	head = nas;
-    head->_suma+=coin_ptr.get_coin_value();
 };
 
 Coin* Stack::pop(){
     if(head!=NULL){
-         Node *temp = head;
+         Coin* ret = head->coin_ptr;
+         Node* temp = head;
 	     head = head->next;
-         return &(temp->coin_ptr);
-    }
+         delete temp;
+         return ret;
+         }
     else return NULL;
     };
 Stack::~Stack(){
-        while(head!=NULL){
-         Node *temp = head;
-	     head = head->next;
-         delete temp;
-    }
+        while(head!=NULL){;
+            Node *temp = head;
+            head = head->next;
+            delete temp->coin_ptr;
+            delete temp;
+        }
 };
         
