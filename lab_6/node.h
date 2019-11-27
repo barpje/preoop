7cbd41bfe14c8a7b9231fda2a6a60a9fd9dbb157
Bartosz Piętka <57501043+barpje@users.noreply.github.com>
2019-11-27 20:51:41 +0100

    Update node.h

--- lab_6/node.h
+++ lab_6/node.h
@@ -1,13 +1,18 @@
 #ifndef node_h
 #define node_h
 #include "coin.h"
+class Stack;
 class Node{
+    //deklaracja przyjazni z klasa Stack;
     friend class Stack;
     public:
+    //metoda statyczna zwracaja calkowita wartosc stosu
         static unsigned get_tot_coins_val();
     private:
-        Coin coin_ptr;
-	    Node *next;
+    //konstruktor przyjmujacy jako argument referecje na Coin
+        Node(const Coin&);
+        Coin *coin_ptr;
+	Node *next;
         static unsigned _suma;
 };
         
